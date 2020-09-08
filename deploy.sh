53cb51eb015bc785adaae555dcbf7bfb6951e02f
aspetter <aspetter@gmail.com>
2020-09-08 23:38:42 +1000

    update travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#build
 docker build -t aspetter/gk8s-client:latest -t aspetter/gk8s-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t aspetter/gk8s-server:latest -t aspetter/gk8s-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t aspetter/gk8s-worker:latest -t aspetter/gk8s-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
@@ -7,6 +8,7 @@ docker push aspetter/gk8s-server:latest
 docker push aspetter/gk8s-server:$GIT_SHA
 docker push aspetter/gk8s-worker:latest
 docker push aspetter/gk8s-worker:$GIT_SHA
+# deploy to kubernetes
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=aspetter/gk8s-server:$GIT_SHA
 kubectl set image deployments/client-deployment client=aspetter/gk8s-client:$GIT_SHA
