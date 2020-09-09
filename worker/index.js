7b31c3cabcbe087d105ee2427a22295192b5a574
aspetter <aspetter@gmail.com>
2020-09-09 21:39:49 +1000

    updated fib and header

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,9 @@
+import { create, all } from 'mathjs';
+const config = { };
+const math = create(all, config);
 const keys = require('./keys');
 const redis = require('redis');
+
 
 const redisClient = redis.createClient({
   host: keys.redisHost,
@@ -8,9 +12,17 @@ const redisClient = redis.createClient({
 });
 const sub = redisClient.duplicate();
 
+//function fib(index) {
+//  if (index < 2) return 1;
+//  return fib(index - 1) + fib(index - 2);
+//}
+
 function fib(index) {
-  if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+    var neo = math.matrix([[1, 1], [1, 0]]);
+    // raise to power of index
+    var morpheus = math.power(neo, index); 
+    // return subset ( top right)
+    return math.subset(morpheus, math.index(0, 1));
 }
 
 sub.on('message', (channel, message) => {
